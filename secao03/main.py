f914b3cfc21285792e08b8b87e48aae08179bf7f
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-07-16 18:38:20 -0300

    Adicionando novos métodos

--- secao03/main.py
+++ secao03/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+from fastapi import Response
 from fastapi import status
+from models import Curso
 
 app = FastAPI()
 
@@ -30,6 +33,36 @@ async def get_curso(id_curso: int):
         return curso
     except KeyError:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado!')
+
+
+@app.post('/cursos', status_code=status.HTTP_201_CREATED)
+async def post_curso(curso: Curso):
+    next_id: int = len(cursos) + 1
+    cursos[next_id] = curso
+    del curso.id    
+    return curso
+
+@app.put('/cursos/{id_curso}')
+async def put_curso(id_curso: int, curso: Curso):
+    if id_curso in cursos:
+        cursos[id_curso] = curso
+        del curso.id
+
+        return curso
+    else:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado!')
+
+@app.delete('/cursos/{id_curso}')
+async def delete_curso(id_curso: int):
+    try:
+        del cursos[id_curso]
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+        #return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
+        #raise HTTPException(status_code=status.HTTP_204_NO_CONTENT)
+    except KeyError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Curso não encontrado')
+
+
 
 
 
